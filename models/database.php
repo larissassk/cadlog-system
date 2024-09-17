4799759e2eb4da6d749adc9c847c82606d731f1f
emillycaaroline <emillyjchagas@gmail.com>
2024-09-17 12:49:17 -0300

    cria padrão Singleton, cujo objetivo é garantir que apenas uma unica instancia de classe seja criada durante a execução do programa, e que essa instancia seja utilizada sempre que necessario

--- models/database.php
+++ models/database.php
@@ -1,5 +1,5 @@
 <?php
 class Database{
-    
+    // Utiliza padrão Singleton, cujo objetivo é garantir que apenas uma unica instancia de classe seja criada durante a execução do programa, e que essa instancia seja utilizada sempre que necessario
 }
 ?>
