c844707d372dd71f10842bc5627a04d39943b0bb
emillycaaroline <emillyjchagas@gmail.com>
2024-09-24 12:25:58 -0300

    implementado ligação do estilo para register (style.css)

--- views/register.php
+++ views/register.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastrar-se</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
@@ -12,13 +13,13 @@
         <h2>Cadastro de usuário</h2>
         <form action="" method="post">
             <label for="nome">Nome</label>
-            <input type="text" name="nome" id="nome" required>
+            <input type="text" name="nome" id="nome" required><br>
 
             <label for="email">Email</label>
-            <input type="email" name="email" id="email" required>
+            <input type="email" name="email" id="email" required><br>
 
             <label for="senha">Senha</label>
-            <input type="password" name="senha" id="senha" required>
+            <input type="password" name="senha" id="senha" required><br>
 
             <label for="perfil">Perfil:</label>
             <select name="perfil" id="perfil">
