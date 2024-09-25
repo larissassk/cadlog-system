6da22540941e73dcb7c9a278acf183b2873751c9
larissassk <larissasantos200702@gmail.com>
2024-09-25 10:10:36 -0300

    Criado função para localizar usuario por email e id

--- models/user.php
+++ models/user.php
@@ -1,9 +1,31 @@
 <?php
 require_once 'model/database.php';
 
-class User{
-    public static function findByEmail($email){ //função que pega os dados 
+class User
+{
+    //Função para localizar usuário pelo email 
+    public static function findByEmail($email)
+    {
+        $conn = Database::getConnection();
+        $stmt = $conn->prepare("SELECT * FROM usuarios WHERE email = :email");
+        $stmt->execute(['email' => $email]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
 
+
+    //Função para encontrar usuario pelo id
+    public static function find($id)
+    {
+        $conn = Database::getConnection();
+        $stmt = $conn->prepare("SELECT * FROM usuarios WHERE id = :id");
+        $stmt->execute(['id' => $id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+    //Função para criar um novo usuario no banco de dados
+    public static function create($data)
+    {
+        $conn = Database::getConnection();
+        $stmt = $conn->prepare("INSERT INTO usuarios(nome, email senha, perfil)");
     }
 }
 ?>
