f52655aba48db464fb77857c57c9e63e3137329e
Larissa Manrique <127845865+larissassk@users.noreply.github.com>
2024-09-30 18:13:31 -0300

    estiliza o arquivo de login

--- views/login.php
+++ views/login.php
@@ -5,21 +5,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Entrar</title>
-    <link rel="stylesheet" href="style.css">
-  
+    <link rel="stylesheet" href="estilo.css">
+
 </head>
 
 <body>
     <!-- O formulário usa o método POST para enviar dados de forma segura -->
     <main>
-        <h2>Login</h2>
-        <form method="post" action="index.php?action=login">
-            <input type="email" name="email" placeholder="Email" required><br>
-            <input type="password" name="senha" placeholder="Senha" required><br>
-            <button type="submit">Login</button>
-        </form>
-        <!-- Define o destino do link e leva à opção de cadastro -->
-       <button><a href="index.php?action=register">Cadastrar-se</a></button> 
+        <div>
+            <h2>Entrar</h2>
+            <form method="post" action="index.php?action=login">
+                <input type="email" name="email" placeholder="Email" required><br>
+                <input type="password" name="senha" placeholder="Senha" required><br>
+                <button type="submit">Login</button>
+            </form>
+            <!-- Define o destino do link e leva à opção de cadastro -->
+            <!-- <button><a href="index.php?action=register">Cadastrar-se</a></button>  -->
+            <a href="index.php?action=register">Cadastrar-se</a>
+
+        </div>
     </main>
 </body>
 
