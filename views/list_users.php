60d6cdd0acc3176c107ae5fbc2c0c1674219b2db
larissassk <larissasantos200702@gmail.com>
2024-10-23 10:16:06 -0300

    Atualiza o css para a lista de usuarios

--- views/list_users.php
+++ views/list_users.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="pt-br">
- 
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista de Usuários</title>
     <link rel="stylesheet" type='text/css' media='screen' href="css/list.css"> <!-- Link para o arquivo CSS -->
 </head>
- 
+
 <body class=""> <!-- Define a classe com base no perfil do usuário -->
     <div class="container">
         <h2>Lista de Usuários</h2>
@@ -22,7 +22,7 @@
                 </tr>
             </thead>
             <tbody>
- 
+
                 <tr>
                     <td></td>
                     <td></td>
@@ -30,13 +30,12 @@
                     <td></td>
                     <td></td>
                 </tr>
- 
+
             </tbody>
         </table>
- 
+
         <a href="" class="btn">Voltar ao Dashboard</a>
     </div>
 </body>
- 
+
 </html>
-tem menu de contexto
