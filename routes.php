22eeb3776a2448a1f13043b1ee554bd684dd5d40
larissassk <larissasantos200702@gmail.com>
2024-11-07 19:57:22 -0300

    Adicionada lógica para excluir usuário na URL com método GET.

--- routes.php
+++ routes.php
@@ -3,16 +3,16 @@
 require 'controllers/AuthController.php'; // inclui o controlador de autenticação
 require 'controllers/UserController.php'; // inclui o controlador de usuário
 require 'controllers/DashboardController.php'; // inclui o controlador de dashboard
- 
+
 // Cria instâncias dos controladores para utilizar seus métodos
 $authController = new AuthController(); // Instancia o controlador de autenticação
 $userController = new UserController();
 $dashboardController = new DashboardController();
- 
- 
+
+
 // Coleta a ação da URL, se não houver definida, usa 'login' por padrão
 $action = $_GET['action'] ?? 'login'; // Usa operador de coalescência nula (??) para definir 'login' se 'action' não estiver presente
- 
+
 // Verifica a ação solicitada e chama o método apropriado do controlador
 switch ($action) {
     case 'login':
@@ -27,13 +27,20 @@ switch ($action) {
     case 'logout':
         $authController->logout();
         break;
-        case 'list':
-            $userController->list();
-            break;
-            case 'edit':
-                $id = $_GET['id'];
-                $userController->edit($id);
-                break;
+    case 'list':
+        $userController->list();
+        break;
+    case 'edit':
+        $id = $_GET['id'];
+        $userController->edit($id);
+        break;
+    case 'delete':
+        if (isset($_GET['id'])) {
+            $userController->delete($_GET['id']);
+            header('Location: index.php?action=list');
+            exit;
+        }
+        break;
     default:
         $authController->login();
         break;
